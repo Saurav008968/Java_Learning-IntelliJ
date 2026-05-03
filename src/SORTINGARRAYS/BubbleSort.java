77515ae01f82f01792a4c152b738a4c39c14bbd3
Saurav <your-email@example.com>
2026-05-04 01:00:51 +0530

    commit..

---
+++ src/SORTINGARRAYS/BubbleSort.java
@@ -0,0 +1,17 @@
+package SORTINGARRAYS;
+import java.util.*;
+public class BubbleSort {
+    static void main() {
+        int [] arr={2,9,39,12,1,76,16,43,5,19};
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.print(Arrays.toString(arr));
+    }
+}
